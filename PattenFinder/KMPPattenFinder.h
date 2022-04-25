5f6aa8a2a648a5df89ca6c57222f66ace61320c1
12UE <1258432472@qq.com>
2022-04-25 23:18:07 +0800

    packet class

---
+++ PattenFinder/KMPPattenFinder.h
@@ -0,0 +1,104 @@
+#pragma once
+class KMPPattenFinder {
+private:
+	struct Pattern {
+		BYTE * m_patten;
+		int m_pattenlength;
+		int m_masklength;
+		LPSTR m_szmask;
+		bool m_Bfind=false;
+		std::vector<int> m_result;
+		Pattern(BYTE * patten, int pattenlength, int masklenfth, LPSTR szmask) {
+			m_patten=patten;
+			m_pattenlength=pattenlength;
+			m_masklength=masklenfth;
+			m_szmask=szmask;
+		}
+	};
+	BYTE * m_pData;
+	int m_DataSize;
+	std::vector<Pattern> m_patterns;
+	//pData:数据指针//result 是搜索结果//end 是搜索的结束位置//begin 是搜索的起始位置//patten 是模式串//next 是模式串的next数组//mask 是模式串的掩码
+	bool FindPatten(PBYTE pData, int begin, int end, PBYTE Patten, int PattenLen, int * next, LPSTR szmask, std::vector<int> & result) {
+		int i=begin;
+		int j=-1;
+		while (i < end && j < PattenLen) {
+			if (j == -1 || szmask[j] != 'x' || pData[i] == Patten[j]) {
+				++i; ++j;
+
+			} else {
+				i=i + j - next[j];//i+j-next[j] 代表着i的下一个位置 这样的好处是如果模式串中出现了重复的字符，那么就可以跳过重复的字符
+				j=next[j];
+			}
+		}
+		if (j >= PattenLen) {
+			result.push_back(i - PattenLen);
+			return FindPatten(pData, i - PattenLen + 1, end, Patten, PattenLen, next, szmask, result);//递归
+		} else {
+			return false;
+		}
+	}
+	bool FindPattenEx(PBYTE pData, PBYTE patten, int datasize, LPSTR szmask, std::vector<int> & result) {
+		auto GetNext=[&] (PBYTE patten, int pattenlength, int * next, LPSTR szMask)->void {
+			int i=0;
+			int j=-1;
+			next[0]=-1;
+			while (i < pattenlength - 1) {
+				if (j == -1 || szMask[i] == '?' || patten[i] == patten[j]) {
+					++i; ++j;
+					if (patten[i] != patten[j] && szMask[i] != '?') {
+						next[i]=j;
+					} else {
+						next[i]=next[j];
+					}
+				} else {
+					j=next[j];
+				}
+			}
+		};
+		auto PattenLen=strlen(szmask);
+		auto next=new int[PattenLen];
+		memset(next, 0, sizeof(int) * PattenLen);
+		GetNext(patten, PattenLen, next, szmask);
+		int parts=6;
+		int segment=datasize / parts;
+#pragma omp parallel for num_threads(parts)
+		for (int i=0; i < parts; i++) {
+			FindPatten(pData, i * segment, segment * (i + 1), patten, PattenLen, next, szmask, result);
+		}
+#pragma omp parallel for num_threads(parts-1)
+		for (int i=0; i < parts - 1; i++) {
+			FindPatten(pData, segment * (i + 1) - PattenLen, segment * (i + 1) + PattenLen, patten, PattenLen, next, szmask, result);
+		}
+		return result.size() > 0;
+	}
+public:
+	KMPPattenFinder(BYTE * pData, int DataSize) {
+		m_pData=pData;
+		m_DataSize=DataSize;
+	}
+	void AddPattern(BYTE * patten, int pattenlength, int masklenfth, LPSTR szmask) {
+		m_patterns.push_back(Pattern(patten, pattenlength, masklenfth, szmask));
+	}
+	void Find() {
+		auto Pfind=[&] (BYTE * data, int datasize, Pattern * p)->void {
+			FindPattenEx(data, p->m_patten, datasize, p->m_szmask, p->m_result);
+			if (p->m_result.size() > 0)
+				p->m_Bfind=true;
+		};
+		std::vector<std::future<void>> futures;
+		for (auto & p : m_patterns) {
+			futures.push_back(std::move(std::async(std::launch::async | std::launch::deferred, Pfind, m_pData, m_DataSize, &p)));
+		}
+	}
+	std::vector<int> GetResult(int index) {
+		return m_patterns[index].m_result;
+	}
+	std::vector<int> GetResult(BYTE * patten) {
+		for (auto & p : m_patterns) {
+			if (memcmp(p.m_patten, patten, p.m_pattenlength) == 0) {
+				return p.m_result;
+			}
+		}
+	}
+};
--- PattenFinder/PattenFinder.cpp
+++ PattenFinder/PattenFinder.cpp
@@ -13,8 +13,9 @@
 #include<future>
 #include<mutex>
 #include"BoyerMoore.h"
+#include"KMPPattenFinder.h"
 using namespace std;
-#define BENCHMARKTIMES 100
+#define BENCHMARKTIMES 10
 DWORD64 ForceFindPatternEx(BYTE *pData,int datasize,BYTE * bMask, char * szMask) {//暴力匹配
 	static const auto bDataCompare=[&] (BYTE * pData, BYTE * bMask, char * szMask)->bool {
 		for (; *szMask; ++szMask, ++pData, ++bMask)
@@ -22,123 +23,17 @@ DWORD64 ForceFindPatternEx(BYTE *pData,int datasize,BYTE * bMask, char * szMask)
 				return false;
 		return (*szMask == NULL);
 	};
-	for (unsigned int i=0; i < datasize; ++i) {//遍历进程内存
+	int result=0;
+#pragma omp parallel for num_threads(4)
+	for (int i=0; i < datasize; ++i) {//遍历进程内存
 		if (bDataCompare((BYTE *)(pData + i), bMask, szMask))
-			return i;
+			result=i;
 	}
-	return 0;
+	return result;
 }
 
 
 
-void GetNext(PBYTE patten, int pattenlength, int * next, LPSTR szMask) {
-	//位序号从0开始
-	
-	int i=0;
-	int j=-1;
-	next[0]=-1;
-	while (i < pattenlength - 1) {
-		if (j == -1 || szMask[i] == '?' || patten[i] == patten[j]) {
-			++i;
-			++j;
-			if (patten[i] != patten[j] && szMask[i] != '?') {
-				next[i]=j;
-			} else {
-				next[i]=next[j];
-			}
-		} else {
-			j=next[j];
-		}
-	}
-	//print next
-	//for (int i=0; i < pattenlength; i++) {
-	//	cout <<dec<< (int)next[i] << " ";
-	//}
-	//cout << endl;
-}	
-//pData:数据指针//result 是搜索结果//end 是搜索的结束位置//begin 是搜索的起始位置//patten 是模式串//next 是模式串的next数组//mask 是模式串的掩码
-bool FindPatten(PBYTE pData, int begin, int end, PBYTE Patten, int PattenLen, int * next, LPSTR szmask, std::vector<int> & result) {
-	
-	int i=begin;
-	int j=-1;
-	while (i < end && j < PattenLen) {
-		if (j == -1 || szmask[j] != 'x' || pData[i] == Patten[j]) {
-			++i;
-			++j;
-		}else {
-			i=i+j-next[j];//i+j-next[j] 代表着i的下一个位置 这样的好处是如果模式串中出现了重复的字符，那么就可以跳过重复的字符
-			j=next[j];
-		}
-	}
-	if (j >= PattenLen) {
-		result.push_back(i - PattenLen);
-		return FindPatten(pData, i - PattenLen + 1, end, Patten, PattenLen, next, szmask, result);//递归
-	} else {
-		return false;
-	}
-}
-
-bool FindPattenEx(PBYTE pData, PBYTE patten, int datasize, LPSTR szmask, std::vector<int> & result) {
-	
-	auto PattenLen=strlen(szmask);
-	auto next=new int[PattenLen];
-	memset(next, 0, sizeof(int) * PattenLen);
-	GetNext(patten, PattenLen, next, szmask);
-	int parts=6;
-	int segment=datasize / parts;
-#pragma omp parallel for num_threads(parts)
-	for (int i=0; i < parts; i++) {
-		FindPatten(pData, i * segment, segment * (i + 1), patten, PattenLen, next, szmask, result);
-	}
-#pragma omp parallel for num_threads(parts-1)
-	for (int i=0; i < parts - 1; i++) {
-		FindPatten(pData, segment * (i + 1) - PattenLen, segment * (i + 1) + PattenLen, patten,PattenLen, next, szmask, result);
-	}
-	return result.size() > 0;
-}
-//\x66\x6a\x42\x1\x0\x0\x0\x98
-
-
-struct Pattern {
-	BYTE * m_patten;
-	int m_pattenlength;
-	int m_masklength;
-	LPSTR m_szmask;
-	bool m_Bfind=false;
-	std::vector<int> m_result;
-	Pattern(BYTE* patten, int pattenlength,int masklenfth,LPSTR szmask) {
-		m_patten=patten;
-		m_pattenlength=pattenlength;
-		m_masklength=masklenfth;
-		m_szmask=szmask;
-	}
-};
-
-class PattenFinder{
-public:
-	BYTE* m_pData;
-	int m_DataSize;
-	std::vector<Pattern> m_patterns;
-	PattenFinder(BYTE* pData, int DataSize) {
-		m_pData=pData;
-		m_DataSize=DataSize;
-	}
-	void AddPattern(BYTE* patten, int pattenlength,int masklenfth,LPSTR szmask) {
-		m_patterns.push_back(Pattern(patten, pattenlength,masklenfth,szmask));
-	}
-	void Find() {
-		auto Pfind=[&](BYTE* data, int datasize, Pattern *p)->void {
-			//cout <<"ThreadID" << std::this_thread::get_id() << endl;
-			FindPattenEx(data, p->m_patten, datasize, p->m_szmask, p->m_result);
-			if(p->m_result.size()>0)
-				p->m_Bfind=true;
-		};
-		std::vector<std::future<void>> futures;
-		for (auto & p : m_patterns) {
-			futures.push_back(std::move(std::async(std::launch::async | std::launch::deferred, Pfind, m_pData, m_DataSize, &p)));
-		}
-	}
-};
 
 int main() {
 	fstream file("memorydump.bin", ios::in | ios::out | ios::binary);
@@ -173,39 +68,15 @@ int main() {
 	SetThreadPriorityBoost(GetCurrentThread(), TRUE);
 	cout << "特征码正确地址 =0x26a6699" << endl;
 	cout << "搜索" <<dec<< BENCHMARKTIMES << "次" << endl;
-	PattenFinder pf(pData, size);
+	KMPPattenFinder pf(pData, size);
 	auto begin=std::chrono::high_resolution_clock::now();
 	for (int i=0; i < BENCHMARKTIMES; i++) {
 		pf.AddPattern(patten, pattenSize, maskSize, szMask);
 	}
-	std::vector<std::future<bool>> rets;
 	pf.Find();
 	auto usetime=std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
 	cout << "KMP搜索耗时：" << dec << usetime << "ms" << endl;
-	//cout << "搜索结果：" << endl;
-	//for (auto & p : pf.m_patterns) {
-	//	if (p.m_Bfind) {
-	//		cout << "模式串：";
-	//		for (int i = 0; i < p.m_pattenlength; i++) {
-	//			cout << "\\x" << hex << (int)p.m_patten[i];
-	//		}
-	//		cout << endl;
-	//		cout << "掩码：";
-	//		for (int i = 0; i < p.m_masklength; i++) {
-	//			cout << p.m_szmask[i];
-	//		}
-	//		cout << endl;
-	//		cout << "结果：";
-	//		for (auto & r : p.m_result) {
-	//			cout << "0x" << hex << r << " ";
-	//		}
-	//		cout << endl;
-	//	}
-	//}
-
-
-
-
+	cout << "搜索结果：0x" <<hex<<pf.GetResult(0)[0] << endl;
 
 	begin=std::chrono::high_resolution_clock::now();
 	int result2;
