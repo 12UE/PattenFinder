eb3e31783b39bb08f4f2193dbf1d5508a0bb5311
12UE <1258432472@qq.com>
2022-04-25 20:07:05 +0800

    mutipuleSearch

--- PattenFinder/PattenFinder.cpp
+++ PattenFinder/PattenFinder.cpp
@@ -10,8 +10,11 @@
 #include<functional>
 #include<chrono>
 #include<thread>
+#include<future>
+#include<mutex>
 #include"BoyerMoore.h"
 using namespace std;
+#define BENCHMARKTIMES 100
 DWORD64 ForceFindPatternEx(BYTE *pData,int datasize,BYTE * bMask, char * szMask) {//暴力匹配
 	static const auto bDataCompare=[&] (BYTE * pData, BYTE * bMask, char * szMask)->bool {
 		for (; *szMask; ++szMask, ++pData, ++bMask)
@@ -26,8 +29,11 @@ DWORD64 ForceFindPatternEx(BYTE *pData,int datasize,BYTE * bMask, char * szMask)
 	return 0;
 }
 
+
+
 void GetNext(PBYTE patten, int pattenlength, int * next, LPSTR szMask) {
 	//位序号从0开始
+	
 	int i=0;
 	int j=-1;
 	next[0]=-1;
@@ -45,13 +51,14 @@ void GetNext(PBYTE patten, int pattenlength, int * next, LPSTR szMask) {
 		}
 	}
 	//print next
-	for (int i=0; i < pattenlength; i++) {
-		cout <<dec<< (int)next[i] << " ";
-	}
-	cout << endl;
+	//for (int i=0; i < pattenlength; i++) {
+	//	cout <<dec<< (int)next[i] << " ";
+	//}
+	//cout << endl;
 }	
 //pData:数据指针//result 是搜索结果//end 是搜索的结束位置//begin 是搜索的起始位置//patten 是模式串//next 是模式串的next数组//mask 是模式串的掩码
 bool FindPatten(PBYTE pData, int begin, int end, PBYTE Patten, int PattenLen, int * next, LPSTR szmask, std::vector<int> & result) {
+	
 	int i=begin;
 	int j=-1;
 	while (i < end && j < PattenLen) {
@@ -72,35 +79,68 @@ bool FindPatten(PBYTE pData, int begin, int end, PBYTE Patten, int PattenLen, in
 }
 
 bool FindPattenEx(PBYTE pData, PBYTE patten, int datasize, LPSTR szmask, std::vector<int> & result) {
-	int PattenLen=strlen(szmask);
-	int * next=new int[PattenLen];
+	
+	auto PattenLen=strlen(szmask);
+	auto next=new int[PattenLen];
 	memset(next, 0, sizeof(int) * PattenLen);
 	GetNext(patten, PattenLen, next, szmask);
-	int begin=0;
-	int end=datasize;
-	//获取系统最大并发量
-	int parts=std::thread::hardware_concurrency()/2;
+	int parts=6;
 	int segment=datasize / parts;
-	//利用openmp 并行搜索
 #pragma omp parallel for num_threads(parts)
 	for (int i=0; i < parts; i++) {
-		{
-			int nBegin=begin + i * segment;
-			int nEnd=segment*(i + 1);
-			FindPatten(pData,nBegin ,nEnd, patten, PattenLen, next, szmask, result);
-		}
+		FindPatten(pData, i * segment, segment * (i + 1), patten, PattenLen, next, szmask, result);
 	}
-	//搜索两个分块交界处的数据 交界处的大小为pattern的大小
 #pragma omp parallel for num_threads(parts-1)
-	for(int i=0;i<parts-1;i++){
-		int nSegBegin=segment*(i+1)-PattenLen;
-		int nSegEnd=begin+segment*(i+1)+PattenLen;
-		FindPatten(pData, nSegBegin, nSegEnd,patten,PattenLen,next,szmask,result);
+	for (int i=0; i < parts - 1; i++) {
+		FindPatten(pData, segment * (i + 1) - PattenLen, segment * (i + 1) + PattenLen, patten,PattenLen, next, szmask, result);
 	}
-	delete[] next;
 	return result.size() > 0;
 }
 //\x66\x6a\x42\x1\x0\x0\x0\x98
+
+
+struct Pattern {
+	BYTE * m_patten;
+	int m_pattenlength;
+	int m_masklength;
+	LPSTR m_szmask;
+	bool m_Bfind=false;
+	std::vector<int> m_result;
+	Pattern(BYTE* patten, int pattenlength,int masklenfth,LPSTR szmask) {
+		m_patten=patten;
+		m_pattenlength=pattenlength;
+		m_masklength=masklenfth;
+		m_szmask=szmask;
+	}
+};
+
+class PattenFinder{
+public:
+	BYTE* m_pData;
+	int m_DataSize;
+	std::vector<Pattern> m_patterns;
+	PattenFinder(BYTE* pData, int DataSize) {
+		m_pData=pData;
+		m_DataSize=DataSize;
+	}
+	void AddPattern(BYTE* patten, int pattenlength,int masklenfth,LPSTR szmask) {
+		m_patterns.push_back(Pattern(patten, pattenlength,masklenfth,szmask));
+	}
+	void Find() {
+		auto Pfind=[&](BYTE* data, int datasize, Pattern *p)->void {
+			//cout <<"ThreadID" << std::this_thread::get_id() << endl;
+			FindPattenEx(data, p->m_patten, datasize, p->m_szmask, p->m_result);
+			if(p->m_result.size()>0)
+				p->m_Bfind=true;
+		};
+		std::vector<std::future<void>> futures;
+		for (auto & p : m_patterns) {
+			auto future=std::async(std::launch::async|std::launch::deferred,Pfind, m_pData, m_DataSize, &p);
+			futures.push_back(std::move(future));
+		}
+	}
+};
+
 int main() {
 	fstream file("memorydump.bin", ios::in | ios::out | ios::binary);
 	if (!file) {
@@ -118,7 +158,7 @@ int main() {
 	PBYTE patten=(PBYTE)"\x66\x6A\x42\x01\x00\x00\x00\x98";
 	//print patten
 	cout << "模式串为：";
-	for (int i=0; i < 8; i++) {
+	for (int i=0; i < 1; i++) {
 		cout <<"\\x" << hex << (int)patten[i];
 	}
 	cout << endl;
@@ -133,27 +173,55 @@ int main() {
 	auto maskSize=8;
 	SetThreadPriorityBoost(GetCurrentThread(), TRUE);
 	cout << "特征码正确地址 =0x26a6699" << endl;
+
+	PattenFinder pf(pData, size);
+	for (int i=0; i < BENCHMARKTIMES; i++) {
+		pf.AddPattern(patten, pattenSize, maskSize,szMask);
+	}
 	std::vector<int> result;
 	auto begin=std::chrono::high_resolution_clock::now();
-	bool bRet=FindPattenEx(pData, patten, size, szMask, result);
+	std::vector<std::future<bool>> rets;
+	pf.Find();
 	auto usetime=std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
 	cout << "KMP搜索耗时：" << dec << usetime << "ms" << endl;
-	if (bRet) {
-		cout << "KMP搜寻结果地址   =0x" << hex << result[0] << endl;
-	} else {
-		cout << "KMP搜寻结果地址   =0x0"<< endl;
+	cout << "搜索结果：" << endl;
+	for (auto & p : pf.m_patterns) {
+		if (p.m_Bfind) {
+			cout << "模式串：";
+			for (int i = 0; i < p.m_pattenlength; i++) {
+				cout << "\\x" << hex << (int)p.m_patten[i];
+			}
+			cout << endl;
+			cout << "掩码：";
+			for (int i = 0; i < p.m_masklength; i++) {
+				cout << p.m_szmask[i];
+			}
+			cout << endl;
+			cout << "结果：";
+			for (auto & r : p.m_result) {
+				cout << "0x" << hex << r << " ";
+			}
+			cout << endl;
+		}
 	}
-		
+
+
+
 
 
 	begin=std::chrono::high_resolution_clock::now();
-	auto result2=ForceFindPatternEx(pData, size, patten, szMask);
+	int result2;
+	for (int i=0; i < BENCHMARKTIMES; i++) {
+		result2=ForceFindPatternEx(pData, size, patten, szMask);
+	}
 	auto usetime2=std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
 	cout << "暴力搜索耗时：" << dec << usetime2 << "ms" << endl;
 	cout << "暴力搜寻结果地址   =0x" << hex << result2 << endl;
 	std::vector<int> result3;
 	begin=std::chrono::high_resolution_clock::now();
-	bool boyret= BoyerMoore((PBYTE)"\x66\x6a\x42\x1\x0\x0\x0\x98",pattenSize,pData,size, result3);
+	for (int i=0; i < BENCHMARKTIMES; i++) {
+		BoyerMoore((PBYTE)"\x66\x6a\x42\x1\x0\x0\x0\x98", pattenSize, pData, size, result3);
+	}
 	auto usetime3=std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
 	cout << "Boyer-Moore搜索耗时：" << dec << usetime3 << "ms" << endl;
 	cout << "Boyer-Moore搜寻结果地址   =0x" << hex << result3[0] << endl;
